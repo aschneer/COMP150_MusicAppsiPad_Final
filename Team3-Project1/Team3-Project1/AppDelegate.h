8c5d7c58d778723c77ebc5b6fadf88c115f96019
japkon <japkon12@gmail.com>
2015-02-16 18:12:11 -0500

    Pushing the initial app

---
+++ Team3-Project1/Team3-Project1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Team3-Project1
+//
+//  Created by Jacob Apkon on 2/16/15.
+//  Copyright (c) 2015 COMP150. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
