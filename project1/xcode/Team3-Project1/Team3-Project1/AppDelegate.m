8a0d2e57bef0df7d65833fc7378977b532373724
japkon <japkon12@gmail.com>
2015-02-16 19:08:35 -0500

    Initialized AppDelegate with libpd

--- project1/xcode/Team3-Project1/Team3-Project1/AppDelegate.m
+++ project1/xcode/Team3-Project1/Team3-Project1/AppDelegate.m
@@ -47,6 +47,10 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    // Turn off the audio controller, and close the PD file
+    _audioController.active = NO;
+    [PdBase closeFile:_patch];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -55,6 +59,8 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    _audioController.active = YES;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
