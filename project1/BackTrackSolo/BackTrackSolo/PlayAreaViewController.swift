9cb5da350026abd071ba38e22a00c590d91b37cc
japkon <japkon12@gmail.com>
2015-03-03 18:17:56 -0500

    Added a semi working pie chart

--- project1/BackTrackSolo/BackTrackSolo/PlayAreaViewController.swift
+++ project1/BackTrackSolo/BackTrackSolo/PlayAreaViewController.swift
@@ -8,22 +8,33 @@
 
 import UIKit
 
-class PlayAreaViewController: UIViewController {
+class PlayAreaViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    let pieLayer = PieLayer()
+    
     struct notes {
         var midiValue : Int
-        
     }
+    
+    private lazy var tapGesture: UITapGestureRecognizer = {
+        let gesture = UITapGestureRecognizer(target: self, action: "handleTap:")
+        gesture.delegate = self
+        return gesture
+    }()
+    
+    private func addGestures() {
+        self.view.addGestureRecognizer(self.tapGesture)
+    }
+    
     var noteArray : [notes] = []
     var numNotes : Int = 0
-    
-//    @IBOutlet var noteArea: XYPieChart!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         var center = NSNotificationCenter.defaultCenter()
         center.addObserver(self, selector: "receiveNotes:", name: "playableNotes", object: nil)
+        self.addGestures()
     }
 
     deinit {
@@ -35,16 +46,36 @@ class PlayAreaViewController: UIViewController {
         if let info = notification.userInfo as? Dictionary<String, [Int]> {
             var noteArray = info["notes"]
             numNotes = noteArray!.count
-            println(numNotes)
-            println(noteArray!)
+            //println(numNotes)
+            //println(noteArray!)
+            
+            pieLayer.frame = CGRectMake(0, 0, 200, 200)
+            view.layer.addSublayer(pieLayer)
+            
+            for var i = 0; i < numNotes; i++ {
+                pieLayer.addValues([PieElement(value: 1, color: randomColor())], animated: true)
+                println(i)
+            }
         }
     }
     
-//    func numberOfSlicesInPieChart(pieChart: XYPieChart) -> UInt {
-//        return UInt(noteArray.count)
-//    }
+    func randomColor() -> UIColor {
+        var randomRed:CGFloat = CGFloat(drand48())
+        
+        var randomGreen:CGFloat = CGFloat(drand48())
+        
+        var randomBlue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    }
+    
+    func handleTap(tap: UITapGestureRecognizer) {
+        var pos: CGPoint = tap.locationInView(tap.view)
+        var tappedSlice: PieElement = self.pieLayer.pieElemInPoint(pos)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+
 }
