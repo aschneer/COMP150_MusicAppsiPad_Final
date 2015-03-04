4df0357495e0378e8d41eea87e6de7c507496ff8
japkon <japkon12@gmail.com>
2015-03-04 17:51:43 -0500

    Added touch feedback and made the pie bigger

--- project1/BackTrackSolo/BackTrackSolo/PlayAreaViewController.swift
+++ project1/BackTrackSolo/BackTrackSolo/PlayAreaViewController.swift
@@ -47,8 +47,9 @@ class PlayAreaViewController: UIViewController, UIGestureRecognizerDelegate {
             var noteArray = info["notes"]
             numNotes = noteArray!.count
             
-            pieLayer.frame = CGRectMake(0, 0, 490, 604)
             view.layer.addSublayer(pieLayer)
+            pieLayer.setMaxRadius(200, minRadius: 50, animated: true)
+            pieLayer.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
             
             for var i = 0; i < numNotes; i++ {
                 pieLayer.addValues([PieElement(value: 1, color: randomColor(noteArray![i]))], animated: true)
@@ -70,20 +71,36 @@ class PlayAreaViewController: UIViewController, UIGestureRecognizerDelegate {
         var center = NSNotificationCenter.defaultCenter()
         
         var pos: CGPoint = tap.locationInView(tap.view)
-        var tappedSlice: PieElement = self.pieLayer.pieElemInPoint(pos)
         
-        /*if tappedSlice == nil {
-                return
-        }*/
+        println(pos)
         
-        var midinote = CGColorGetComponents(tappedSlice.color.CGColor)[2] * 100
-        var slice_color = CGColorGetComponents(tappedSlice.color.CGColor)
-        tappedSlice.color = UIColor(red: slice_color[0], green: slice_color[1], blue: slice_color[2], alpha: 1)
+        var tappedSlice: PieElement? = self.pieLayer.pieElemInPoint(pos)
         
-        center.postNotificationName("noteToPlay", object: nil, userInfo: ["play": midinote])
+        if let actualSlice = tappedSlice {
+            println("got slice")
         
-        if tap.state != UIGestureRecognizerState.Ended {
-            tappedSlice.color = UIColor(red: slice_color[0], green: slice_color[1], blue: slice_color[2], alpha: 0.75)
+            var midinote = CGColorGetComponents(actualSlice.color.CGColor)[2] * 100
+            var slice_color = CGColorGetComponents(actualSlice.color.CGColor)
+            actualSlice.color = UIColor(red: slice_color[0], green: slice_color[1], blue: slice_color[2], alpha: 1)
+        
+            center.postNotificationName("noteToPlay", object: nil, userInfo: ["play": midinote])
+        
+            if tap.state == UIGestureRecognizerState.Ended {
+                var timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: "fadeOutSlice:", userInfo: ["slice": actualSlice], repeats: false)
+            }
+        }
+    }
+    
+    func fadeOutSlice(timer: NSTimer) {
+        var center = NSNotificationCenter.defaultCenter()
+
+        if let info = timer.userInfo as? Dictionary<String, PieElement> {
+            var slice = info["slice"]
+            
+            var midinote = CGColorGetComponents(slice!.color.CGColor)[2] * 100
+            
+            var slice_color = CGColorGetComponents(slice!.color.CGColor)
+            slice!.color = UIColor(red: slice_color[0], green: slice_color[1], blue: slice_color[2], alpha: 0.75)
             center.postNotificationName("stopNote", object: nil, userInfo: ["stop": midinote])
         }
     }
