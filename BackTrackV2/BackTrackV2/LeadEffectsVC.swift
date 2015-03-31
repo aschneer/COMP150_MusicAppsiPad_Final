1ff086dc0489ea2a373a9109c6a7c6ac1ef4c382
japkon <japkon12@gmail.com>
2015-03-31 19:01:54 -0400

    Added lead sound editor to the main screen

---
+++ BackTrackV2/BackTrackV2/LeadEffectsVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeadEffectsVC.swift
+//  BackTrackV2
+//
+//  Created by Jacob Apkon on 3/31/15.
+//  Copyright (c) 2015 COMP150. All rights reserved.
+//
+
+import UIKit
+
+class LeadEffectsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
